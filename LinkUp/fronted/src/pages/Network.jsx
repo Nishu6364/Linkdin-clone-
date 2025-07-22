5106460b2283795b197960cf7080c5d97316e370
Rakesh Kumar <154679938+RakeshPatelKumar@users.noreply.github.com>
2025-07-22 13:57:20 +0530

    Update Network.jsx

--- LinkUp/fronted/src/pages/Network.jsx
+++ LinkUp/fronted/src/pages/Network.jsx
@@ -8,7 +8,7 @@ import { FaRegCheckCircle } from "react-icons/fa";
 import { RxCrossCircled } from "react-icons/rx";
 import io from "socket.io-client"
 
-const socket=io("http://localhost:8000")
+const socket=io("https://linkup-backend-blwa.onrender.com")
 function Network() {
     let { serverUrl } = useContext(authDataContext);
     let [connection, setConnection] = useState([]);

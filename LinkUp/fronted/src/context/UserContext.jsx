0c41129dc1ee19d90b660229910691b643d32b90
Rakesh Kumar <154679938+RakeshPatelKumar@users.noreply.github.com>
2025-07-22 12:20:46 +0530

    Update UserContext.jsx

--- LinkUp/fronted/src/context/UserContext.jsx
+++ LinkUp/fronted/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
  import { useNavigate } from 'react-router-dom'
 export const userDataContext=createContext()
 import {io} from "socket.io-client"
-export let socket=io("http://localhost:4000")
+export let socket=io("https://linkup-backend-blwa.onrender.com")
 
 function UserContext({children}) {
 let [userData,setUserData]=useState(null)
